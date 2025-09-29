c960b8c1df5c2873c430eefbd4c3bbd7510245bd
SulthanRaghib <raghib.smp@gmail.com>
2025-09-29 22:31:31 +0700

    feat: add next-sitemap configuration and update package dependencies

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+/** @type {import('next-sitemap').IConfig} */
+export const siteUrl = "https://portofolio-raghib.netlify.app/";
+export const generateRobotsTxt = true;
+export const sitemapSize = 5000;
